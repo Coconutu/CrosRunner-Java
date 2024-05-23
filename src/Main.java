02b4168eebc4b89f675ca2fb74cbc41276aef516
Coconutu <sebastian.daniel.adrian@gmail.com>
2024-05-23 12:30:19 +0300

    Definire clase Conurent si Cros

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,35 @@
 import javax.swing.*;
-import java.util.ArrayList;
+import java.awt.*;
+
 
 public class Main {
     static JFrame fereastra;
-    static JPanel panou;
-    static JLabel eticheta;
+    static Font fontulMeu=new Font("Arial",Font.BOLD,16);
+    static JLabel etichetaNumePrenume;
+    static JPanel panouControale,panouAfisaj;
     public static void main(String[] args) {
-        fereastra=new JFrame("Inscriere concurenti");
+        fereastra=new JFrame("Cros copii - Cermei 2024");
+        fereastra.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fereastra.setLocationRelativeTo(null);
+        fereastra.setVisible(true);
+        fereastra.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        panouControale=new JPanel();
+        panouControale.setBackground(Color.BLUE);
+        panouControale.setBounds(0,0,fereastra.getWidth(),300);
+
+        panouAfisaj=new JPanel();
+        panouAfisaj.setBackground(Color.cyan);
+
+        fereastra.add(panouControale);
+        fereastra.add(panouAfisaj);
+
+        etichetaNumePrenume=new JLabel("Nume / Prenume");
+        etichetaNumePrenume.setFont(fontulMeu);
+        etichetaNumePrenume.setBounds(0,0,20,1);
+        panouControale.add(etichetaNumePrenume);
+
+
 
 
 
